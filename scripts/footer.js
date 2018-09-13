449549d890453a4cff703432a88461f5a9299899
julianjca <julianjca11@gmail.com>
2018-09-13 17:07:32 +0700

    finish footer component

---
+++ scripts/footer.js
@@ -0,0 +1,11 @@
+Vue.component('main-footer',{
+  template :`
+  <div class="footer">
+  <h3>join us today</h3>
+  <h3 id="register-footer">register</h3>
+  <h4>
+    Copyright © 2088 musicgram
+  </h4>
+</div>
+  `
+});
