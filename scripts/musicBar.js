fdd298358a64ec7090274c272a8d2a686d50ebc7
julianjca <julianjca11@gmail.com>
2018-09-14 13:21:56 +0700

    add share facebook and twitter

--- scripts/musicBar.js
+++ scripts/musicBar.js
@@ -8,6 +8,13 @@ Vue.component('music-bar',{
         <source :src="music.file" type="audio/mpeg">
         Your browser dose not Support the audio Tag
       </audio>
+      <div class="socmedbutton">
+      <a :href="\'https://twitter.com/intent/tweet?text=\'+music.file" class="twitter-share-button" data-show-count="false"><img src="http://goinkscape.com/wp-content/uploads/2015/07/twitter-logo-final.png"></a>
+
+      <div class="fb-share-button" :data-href="\'music.file'\" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" :href="\'https://www.facebook.com/sharer/sharer.php?u=\'+music.file" class="fb-xfbml-parse-ignore"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/2000px-F_icon.svg.png"></a></div>
+      </div>
+
+
     </div>
 
   `,
